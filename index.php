a9dec3e6ea37375d5e9d9f065846e812f1f9ddc6
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-12-05 17:32:37 +0400

    add content

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <title>Neverland</title>
 </head>
+
 <body>
+
+    <div class="title">
+        <h1>Neverland</h1>
+        <p>where the lost are found</p>
+    </div>
+
+    <div class="enter">
+        <button type="button">Sign Up</button>
+        <button type="button">Log In</button>
+    </div>
     
 </body>
+
 </html>
