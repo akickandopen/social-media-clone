87fe935b9c73ab9b2942fc3ad3db49d418a7f05e
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-12-10 17:34:53 +0400

    connect to database

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?php require 'server.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
+    $connect = mysqli_connect("localhost", "root", "", "neverland_db");
 
-
-
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect: " . mysqli_connect_errno();
+    }
 ?>
