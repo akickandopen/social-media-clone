c9e604ff654216f6ef6f0ee2533855e68c74c882
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-12-27 17:12:55 +0400

    change database

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-    $connect = mysqli_connect("localhost", "root", "", "connect_db");
+    $connect = mysqli_connect("localhost", "root", "", "bond_db");
 
     if(mysqli_connect_errno()) {
         echo "Failed to connect: " . mysqli_connect_errno();
