5155201250643ea9e87448e4ec154262889b0e37
akilahlapure <71811443+akilahlapure@users.noreply.github.com>
2021-12-27 17:14:21 +0400

    create register form

--- register.php
+++ register.php
@@ -1,4 +1,53 @@
-<?php require 'server.php' ?>
+<?php require 'server.php'; 
+
+// Variables
+$fname = ""; //first name
+$lname = ""; //last name
+$email = ""; //email address
+$pass = ""; //password
+$pass2 = ""; //confirm password
+$date = ""; //date signed up
+$error = ""; //error messages
+
+if(isset($_POST['sign_up_btn'])){
+
+    // Remove html tags from form values
+    $fname = strip_tags($_POST['sign_up_fname']); // remove html tags
+    $lname = strip_tags($_POST['sign_up_lname']); 
+    $email = strip_tags($_POST['sign_up_email']); 
+    $pass = strip_tags($_POST['sign_up_pass']);
+    $pass2 = strip_tags($_POST['sign_up_pass2']); 
+
+    $date = date("Y-m-d"); //current date
+    
+    // Check if email is in valid format
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+        // If email already exists
+        $email_check = mysqli_query($connect, "SELECT email FROM users WHERE email='$email'");
+        $num_rows = mysqli_num_rows($email_check); // count no. of rows from $email_check
+
+        if($num_rows > 0){
+            echo "Email already in use.";
+        }
+
+
+    }
+    else {
+        echo "Invalid email format";
+    }
+
+
+
+    // Check if passwords match
+    if($pass != $pass2){
+        echo "Passwords don't match.";
+    } 
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +59,16 @@
     <title>Registration</title>
 </head>
 
-<body style="background-color: blue;">
+<body>
+
+    <form action="register.php" method="POST">
+        <input type="text" name="sign_up_fname" placeholder="First Name" required> <br>
+        <input type="text" name="sign_up_lname" placeholder="Last Name" required> <br>
+        <input type="email" name="sign_up_email" placeholder="Email" required> <br>
+        <input type="password" name="sign_up_pass" placeholder="Password" required> <br>
+        <input type="password" name="sign_up_pass2" placeholder="Confirm Password" required> <br>
+        <input type="submit" name="sign_up_btn" value="Create an account"> 
+    </form>
 
 </body>
 
