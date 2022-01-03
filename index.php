65da8c3ec2aff9f92b1ad7cf99ac8ab8f8228ca1
akickandopen <akilah.lapure@gmail.com>
2022-01-03 19:18:07 +0400

    add newsfeed form

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php include("include_php/nav_header.php"); ?>
 
-
+    <div class="newsfeed">
+        <form action="index.php" class="post-form" method="POST">
+            <textarea name="post-text-area" id="post-text-area" cols="50" rows="5" placeholder="Write a post..."></textarea>
+            <input type="submit" name="post" id="post-submit-btn" value="Post">
+        </form>
+    </div>
 </body>
 
 </html>
