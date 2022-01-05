4a4f8917f8c25327fa784e6bbe2e5f8d64e80904
akickandopen <akilah.lapure@gmail.com>
2022-01-05 18:08:26 +0400

    add wrapper for cards

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php include("include_php/nav_header.php"); ?>
 
-    <div class="newsfeed">
+<div class="wrapper">
+
+    <div class="trending card">
+        <!--for trending words-->
+    </div>
+
+    <div class="newsfeed card">
         <form action="index.php" class="post-form" method="POST">
-            <textarea name="post-text-area" id="post-text-area" cols="50" rows="5" placeholder="Write a post..."></textarea>
+            <textarea name="post-text-area" id="post-text-area" placeholder="Write a post..."></textarea>
             <input type="submit" name="post" id="post-submit-btn" value="Post">
         </form>
     </div>
+
+    <div class="card">
+        <!--additional card-->
+    </div>
+</div>
+
 </body>
 
 </html>
