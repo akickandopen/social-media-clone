b9c4b22b8eacebe8b938bc68ab6c2fca2bb3f3c1
akickandopen <akilah.lapure@gmail.com>
2022-01-06 18:22:06 +0400

    transfer card class to post-form

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
         <!--for trending words-->
     </div>
 
-    <div class="newsfeed card">
-        <form action="index.php" class="post-form" method="POST">
+    <div class="newsfeed">
+        <form action="index.php" class="post-form card" method="POST">
             <textarea name="post-text-area" id="post-text-area" placeholder="Write a post..."></textarea>
             <input type="submit" name="post" id="post-submit-btn" value="Post">
         </form>
