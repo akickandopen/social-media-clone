1159fe29a5f8f48f9bb680ec6c9aa0ed918c284c
akickandopen <akilah.lapure@gmail.com>
2022-01-18 01:21:35 +0400

    create loadPostsFriends() function

--- include_php/classes/Post.php
+++ include_php/classes/Post.php
@@ -150,6 +150,123 @@
 
             echo $post_str;
         }
+
+        public function loadPostsFriends(){
+            $post_str = "";
+            $userLoggedIn = $this->user_obj->getUserID();
+
+            //select posts that haven't been deleted in descending order
+            $posts_data = mysqli_query($this->connect, "SELECT * FROM posts WHERE post_deleted='no' ORDER BY id DESC");
+
+            while($row = mysqli_fetch_array($posts_data)){
+                $id = $row['id'];
+                $body = $row['body'];
+                $user_by_id = $row['user_by_id'];
+                $user_by = $row['user_by'];
+                $date_time = $row['date_added'];
+
+                //Check if user who added the post is closed
+                $user_by_obj = new User($this->connect, $user_by_id);
+                if($user_by_obj->isClosed()){
+                    continue;
+                }
+
+                $user_logged_obj = new User($this->connect, $userLoggedIn);
+                if($user_logged_obj->isFriend($user_by)) {
+
+                    //select user's first name, last name, and profile pic
+                    $user_details_query = mysqli_query($this->connect, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$user_by'");
+                    $user_row = mysqli_fetch_array($user_details_query);
+
+                    $first_name = $user_row['first_name'];
+                    $last_name = $user_row['last_name'];
+                    $profile_pic = $user_row['profile_pic'];
+
+                    //timeframe
+                    $date_time_now = date("Y-m-d H:i:s");
+                    $start_date = new DateTime($date_time); // date post was added
+                    $end_date = new DateTime($date_time_now); // current date and time now
+                    $interval = $start_date->diff($end_date); // difference between start and end date
+
+                    $month_interval = $interval->m;
+                    $day_interval = $interval->d;
+                    $hour_interval = $interval->h;
+                    $min_interval = $interval->i;
+                    $sec_interval = $interval->s;
+
+                    if($month_interval >= 1){ // if interval is more than or equal to 1 month
+
+                        if($day_interval == 0){ // if interval is exactly one month
+                            $days = " ago"; 
+                        } 
+                        else if($day_interval == 1) { 
+                            $days = $interval->d . " day ago"; // add "1 day ago"
+                        }
+                        else { 
+                            $days = $day_interval . " days ago"; // add "1+ days ago"
+                        }
+
+                        if($month_interval == 1){ // if interval is one month ago
+                            $time_interval_message = $month_interval . " month" . $days;
+                        } else { // more than one month
+                            $time_interval_message = $month_interval . " months" . " and" . $days;
+                        }
+                    }
+                    else if($day_interval >= 1){ // if interval is more than or equal to 1 day
+
+                        if($day_interval == 1) { 
+                            $time_interval_message = "Yesterday"; 
+                        }
+                        else { 
+                            $time_interval_message = $day_interval . " days ago";
+                        }
+                    }
+                    else if($hour_interval >= 1){ // if interval is more than or equal to 1 hour
+                        
+                        if($hour_interval == 1) { 
+                            $time_interval_message = $hour_interval . " hour ago"; 
+                        }
+                        else { 
+                            $time_interval_message = $hour_interval . " hours ago";
+                        }
+                    }
+                    else if($min_interval >= 1){ // if interval is more than or equal to 1 minute
+                        
+                        if($min_interval == 1) { 
+                            $time_interval_message = $min_interval . " minute ago"; 
+                        }
+                        else { 
+                            $time_interval_message = $min_interval . " minutes ago"; 
+                        }
+                    }
+                    else{ 
+
+                        if($sec_interval < 15) { // if interval is less than 15 seconds
+                            $time_interval_message = "Just now";
+                        }
+                        else { 
+                            $time_interval_message = $sec_interval . " seconds ago";
+                        }
+                    }
+
+                    $post_str .= "<div class='status-post card'>
+                                    <div class='post-details'>
+                                        <div class='post-details-profile'>
+                                            <img src='$profile_pic' width='36' alt='Profile Picture'>
+                                            <a href='$user_by'> $first_name $last_name </a>
+                                        </div>
+                                        <div class='post-details-time'>
+                                            $time_interval_message
+                                        </div>
+                                    </div>
+                                    <div id='post-body'>
+                                        $body <br>
+                                    </div>
+                                </div>";
+                }
+            }
+            echo $post_str;
+        }
     }
 
 ?>
