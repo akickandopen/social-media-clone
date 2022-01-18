318dedd49c66c09a53361ab4b8f35cd7e30bf895
akickandopen <akilah.lapure@gmail.com>
2022-01-18 16:06:29 +0400

    remove limit

--- include_php/ajax_loading_posts.php
+++ include_php/ajax_loading_posts.php
@@ -1,10 +1,8 @@
 <?php
-    include("../server.php"); // database
+    include("server.php"); // database
     include("classes/User.php");
-    include("classes/Post.php")
+    include("classes/Post.php");
 
-    $limit = 5; //number limit of posts to be loaded
-
-    $posts = new Post($connect, $_REQUEST['userLoggedIn']);
-    $posts->loadPosts();
+    $post = new Post($connect, $_REQUEST['userLoggedIn']);
+    $post->loadPostsAll();
 ?>
