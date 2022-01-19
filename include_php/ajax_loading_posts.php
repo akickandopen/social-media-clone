35cbd8c81cbed0760bd1b6c4ed95d6248fde72ad
akickandopen <akilah.lapure@gmail.com>
2022-01-20 01:29:35 +0400

    delete loading php

--- include_php/ajax_loading_posts.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("server.php"); // database
-    include("classes/User.php");
-    include("classes/Post.php");
-
-    $post = new Post($connect, $_REQUEST['userLoggedIn']);
-    $post->loadPostsAll();
-?>
