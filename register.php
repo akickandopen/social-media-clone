a97b185eb1c9f1022e3edeef787b33f3bbdcb517
akickandopen <akilah.lapure@gmail.com>
2022-01-20 14:54:38 +0400

    change layout

--- register.php
+++ register.php
@@ -11,68 +11,139 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="resources/css/register_style.css">
     <title>Registration</title>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="resources/css/register_style.css">
 </head>
 
 <body>
 
-    <header>
-        <h1>Bond</h1>
-    </header>
+    <div class="container">
+      <div class="forms-container">
+        <div class="login-signup">
+            <form action="register.php" method="POST" class="log-in-form">
+                <h2 class="title">Log in</h2>
 
-    <section>
-        <div class="tabs">
-            <input type="radio" name="tab-ex" id="tab-sign-up" class="tab-radio" checked>
-            <label for="tab-sign-up" class="tab-label">Log In</label>
-            <div class="tab-content">
-                <form action="register.php" method="POST">
+                <div class="input-field">
+                    <i class="material-icons">email</i>
                     <input type="email" name="login_email" placeholder="Email" value="<?php
                         if(isset($_SESSION['login_email'])){
                             echo $_SESSION['login_email'];
-                        }?>" required> <br>
-                    <input type="password" name="login_pass" placeholder="Password" required> <br>
+                        }?>" required>
+                </div>
 
-                    <input type="submit" name="login_btn" value="Login"> <br>
-                    <?php if(in_array("Email or password is incorrect.", $error_list)) echo "Email or password is incorrect.<br>"; ?>
-                </form>
-            </div>
+                <div class="input-field">
+                    <i class="material-icons">lock</i>
+                    <input type="password" name="login_pass" placeholder="Password" required>
+                </div>
 
-            <input type="radio" name="tab-ex" id="tab-login" class="tab-radio">
-            <label for="tab-login" class="tab-label">Sign Up</label>
-            <div class="tab-content">
-                <form action="register.php" method="POST">
+                <input type="submit" name="login_btn" value="Login" class="btn solid">
+                <?php if(in_array("Email or password is incorrect.", $error_list)) echo "Email or password is incorrect.<br>"; ?>
+            </form>
+            <form action="register.php" method="POST" class="sign-up-form">
+                <h2 class="title">Sign up</h2>
+
+                <div class="input-field">
+                    <i class="material-icons">person</i>
                     <input type="text" name="sign_up_fname" placeholder="First Name" value="<?php
                         if(isset($_SESSION['sign_up_fname'])){
                             echo $_SESSION['sign_up_fname'];
-                        }?>" required> <br>
+                        }?>" required>
+                </div>
 
+                <div class="input-field">
+                    <i class="material-icons">person</i>
                     <input type="text" name="sign_up_lname" placeholder="Last Name" value="<?php
                         if(isset($_SESSION['sign_up_lname'])){
                             echo $_SESSION['sign_up_lname'];
-                        }?>" required> <br>
+                        }?>" required>
+                </div>
 
+                <div class="input-field">
+                    <i class="material-icons">email</i>
                     <input type="email" name="sign_up_email" placeholder="Email" value="<?php
                         if(isset($_SESSION['sign_up_email'])){
                             echo $_SESSION['sign_up_email'];
-                        }?>" required> <br>
+                        }?>" required>
+
                     <?php 
                         if(in_array("Email already in use.", $error_list)) echo "Email already in use.<br>";
                         else if (in_array("Invalid email format.", $error_list)) echo "Invalid email format.<br>";
                     ?>
+                </div>
 
-                    <input type="password" name="sign_up_pass" placeholder="Password" required> <br>
-                    <input type="password" name="sign_up_pass2" placeholder="Confirm Password" required> <br>
+                <div class="input-field">
+                    <i class="material-icons">lock</i>
+                     <input type="password" name="sign_up_pass" placeholder="Password" required>
+                </div>
+
+                <div class="input-field">
+                    <i class="material-icons">lock</i>
+                    <input type="password" name="sign_up_pass2" placeholder="Confirm Password" required>
                     <?php if(in_array("Passwords don't match.", $error_list)) echo "Passwords don't match.<br>"; ?>
+                </div>
 
-                    <input type="submit" name="sign_up_btn" value="Create an account">
-                </form>
-            </div>
+                    <input type="submit" name="sign_up_btn" value="Create an account" class="btn">
+                </div>
+            </form>
         </div>
 
-        <img src="resources/images/website/people.png" alt="People" class="img-people">
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>New here?</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!
+            </p>
+            <button class="btn transparent" id="sign-up-btn">
+              Sign up
+            </button>
+          </div>
+          <img src="resources/images/website/register.svg" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>Already have an account?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button class="btn transparent" id="log-in-btn">
+              Log in
+            </button>
+          </div>
+          <img src="resources/images/website/login.svg" class="image" alt="" />
+        </div>
+      </div>
+    </div>
+    
 
-    </section>
+    <script>
+        var log_in_btn = document.querySelector("#log-in-btn");
+        var sign_up_btn = document.querySelector("#sign-up-btn");
+        var container = document.querySelector(".container");
+
+        // var x = document.getElementById("toggleProfile");
+        //     if (x.style.display == "block") {
+        //         x.style.display = "none";
+        //     } else {
+        //         x.style.display = "block";
+        //     }
+
+        sign_up_btn.addEventListener("click", () => {
+            container.classList.add("sign-up-mode");
+
+
+        });
+
+        log_in_btn.addEventListener("click", () => {
+            container.classList.remove("sign-up-mode");
+
+
+        });
+    </script>
 
 
 </body>
